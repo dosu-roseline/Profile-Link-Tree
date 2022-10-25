54ed3aeea04ad3bc50ad478b11db4fc22c77be32
roseline_dosu <dosuroseline@gmail.com>
2022-10-25 18:57:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom'
+import user from './img/rose.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="container">
+   <div className="profile">
+    <img src={user} alt="User Img" className='userImg' />
+    <h1 id='twitter'>dosu-roseline</h1>
+    <h1 id='slack'>Slack</h1>
+   </div>
+  <div className="links">
+    <a href="https://twitter.com/DosuRoseline" target='blank' id='twitter__link'>Twitter Link</a>
+    <a href="https://training.zuri.team/" id="btn__zuri" target='blank'>Zuri Team</a>
+    <a href="http://books.zuri.team " id="books" target='blank'>Zuri Books</a>
+    <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>" id="book__python" target='blank'>Python Books</a>
+    <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>" id="pitch" target='blank'>Background Check for Coders</a>
+    <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>" id="book__design" target='blank'>Design Books</a>
+  </div>
+  </div>
 }
 
 export default App;
